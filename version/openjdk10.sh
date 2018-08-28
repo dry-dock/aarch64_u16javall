59095967de7e1e01731493b2577df74cea0deb3c
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-08-28 14:05:30 +0530

    Update openjdk10.sh

--- version/openjdk10.sh
+++ version/openjdk10.sh
@@ -4,3 +4,7 @@ echo "================= Installing openjdk-10-jdk ==================="
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update -qq
 apt-get install -qq -y openjdk-10-jdk
+
+echo 'export JAVA_HOME=/usr/lib/jvm/java-8-oracle' >> /etc/drydock/.env
+echo 'export PATH=$PATH:/usr/lib/jvm/java-8-oracle/bin' >> /etc/drydock/.env
+
