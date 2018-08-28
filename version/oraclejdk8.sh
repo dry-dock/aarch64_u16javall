11134b703f0650c1bb31713fc5d6f67b0027b3f8
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-08-28 14:05:04 +0530

    Update oraclejdk8.sh

--- version/oraclejdk8.sh
+++ version/oraclejdk8.sh
@@ -10,5 +10,3 @@ apt-get install -y oracle-java8-installer
 update-alternatives --set java /usr/lib/jvm/java-8-oracle/jre/bin/java
 update-alternatives --set javac /usr/lib/jvm/java-8-oracle/bin/javac
 
-echo 'export JAVA_HOME=/usr/lib/jvm/java-8-oracle' >> /etc/drydock/.env
-echo 'export PATH=$PATH:/usr/lib/jvm/java-8-oracle/bin' >> /etc/drydock/.env
