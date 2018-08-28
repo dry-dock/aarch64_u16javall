81d48fcaa4fa4869cd2b28de665cc79a2f7ce1b2
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-08-28 12:47:32 +0530

    Update openjdk10.sh

--- version/openjdk10.sh
+++ version/openjdk10.sh
@@ -1,12 +1,9 @@
-
 #!/bin/bash -e
-echo "================ Installing openjdk-10-jdk ================="
-export OPENJDK_VERSION="10.0.2"
-mkdir -p /usr/lib/jvm && cd /usr/lib/jvm
-wget "https://download.java.net/java/GA/jdk10/$OPENJDK_VERSION/19aef61b38124481863b1413dce1855f/13/openjdk-"$OPENJDK_VERSION"_linux-x64_bin.tar.gz"
-tar -xzf openjdk-"$OPENJDK_VERSION"_linux-x64_bin.tar.gz
-mv jdk-"$OPENJDK_VERSION"/ java-10-openjdk-amd64
 
+echo "================= Installing openjdk-10-jdk ==================="
+add-apt-repository -y ppa:openjdk-r/ppa
+apt-get update -qq
+apt-get install -qq -y openjdk-10-jdk
 sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-10-openjdk-amd64/bin/java 1
 sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-10-openjdk-amd64/bin/javac 1
 sudo update-alternatives --set java /usr/lib/jvm/java-10-openjdk-amd64/bin/java
