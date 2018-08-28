764f29288acebea2a2a9ad4fe0b262f7e181daf7
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-08-28 13:48:11 +0530

    Update openjdk10.sh

--- version/openjdk10.sh
+++ version/openjdk10.sh
@@ -4,7 +4,3 @@ echo "================= Installing openjdk-10-jdk ==================="
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update -qq
 apt-get install -qq -y openjdk-10-jdk
-sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-10-openjdk-amd64/bin/java 1
-sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-10-openjdk-amd64/bin/javac 1
-sudo update-alternatives --set java /usr/lib/jvm/java-10-openjdk-amd64/bin/java
-sudo update-alternatives --set javac /usr/lib/jvm/java-10-openjdk-amd64/bin/javac
