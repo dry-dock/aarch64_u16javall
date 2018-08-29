5951cd63c7a60e09aa35d403b45cea7b668415b6
Ranjansing41 <ranjan@shippabl.com>
2018-08-29 15:24:31 +0530

    fixing bug in path

--- version/openjdk10.sh
+++ version/openjdk10.sh
@@ -5,6 +5,6 @@ add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update -qq
 apt-get install -qq -y openjdk-10-jdk
 
-echo 'export JAVA_HOME=/usr/lib/jvm/java-10-openjdk' >> /etc/drydock/.env
-echo 'export PATH=$PATH:/usr/lib/jvm/java-10-openjdk/bin' >> /etc/drydock/.env
+echo 'export JAVA_HOME=/usr/lib/jvm/java-10-openjdk-arm64' >> /etc/drydock/.env
+echo 'export PATH=$PATH:/usr/lib/jvm/java-10-openjdk-arm64/bin' >> /etc/drydock/.env
 
